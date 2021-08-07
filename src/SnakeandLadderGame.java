82c5ccbdb7ea87f9ef755415494a4dc05d1738c9
Gaurip10 <thegauriparhad@gmail.com>
2021-08-07 16:16:05 +0530

    starting game with position 0

--- src/SnakeandLadderGame.java
+++ src/SnakeandLadderGame.java
@@ -2,6 +2,9 @@ public class SnakeandLadderGame {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Snake and Ladder program");
+
+        int Position=0;
+        int Player=1;
     }
 
 }
