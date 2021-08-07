98994daee9d63e47ddc97fdb013ca32ccdab0afa
Gaurip10 <thegauriparhad@gmail.com>
2021-08-07 16:30:49 +0530

    Rolling a die

--- src/SnakeandLadderGame.java
+++ src/SnakeandLadderGame.java
@@ -2,9 +2,13 @@ public class SnakeandLadderGame {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Snake and Ladder program");
-
         int Position=0;
         int Player=1;
+        System.out.println("Position:"+Position);
+        int randomcheck=(int) Math.floor(Math.random()+1)%6;
+        System.out.println("Dice number is:"+randomcheck);
+
+
     }
 
 }
